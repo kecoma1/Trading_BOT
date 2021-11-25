52dd4709826eddd39793c93f36c7327d182f03fb
kecoma1 <kecoma2@gmail.com>
2021-11-25 12:41:08 +0100

    More tutorials

---
+++ tutoriales/Python/sort.py
@@ -0,0 +1,25 @@
+import random
+from faker import Faker
+
+# Just for random strings
+fake = Faker()
+
+# Generating the list
+nums = [random.randint(0, 10) for _ in range(10)]
+print("Unsorted list:", nums)
+nums = sorted(nums)
+print("Sorted list:", nums)
+
+# List of tuples
+tuples = [(fake.first_name(), random.randint(0, 10)) for _ in range(10)]
+print("Unsorted list:", tuples)
+# sorted(tuples)  # ERROR!
+tuples = sorted(tuples, key=lambda tuple: tuple[1])
+print("Sorted list:", tuples)
+
+# Sorting values of a dictionary
+dictionary = {fake.first_name(): random.randint(0, 10) for _ in range(10)}
+print("Unsorted dictionary:", dictionary)
+# dictionary = sorted(dictionary.values())
+dictionary = sorted(dictionary.items(), key=lambda tuple: tuple[1])
+print("Sorted dictionary:", dictionary)
---
+++ tutoriales/Python/zip_enum.py
@@ -0,0 +1,28 @@
+import enum
+from faker import Faker
+
+# Just for random strings
+fake = Faker()
+
+l1 = [i for i in range(10)]
+l2 = [fake.first_name() for _ in range(10)]
+
+l3 = []
+d = {}
+
+for number, name in zip(l1, l2):
+    l3.append((number, name))
+    d[name] = number
+
+print("New list:", l3)
+print("New dict:", d)
+
+# New names
+l2 = [fake.first_name() for _ in range(10)]
+enumerated = []
+
+# Lets enumerate them
+for i, name in enumerate(l2):
+    enumerated.append((i, name))
+
+print("Enumerated names:", enumerated)
