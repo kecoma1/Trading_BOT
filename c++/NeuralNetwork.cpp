1ed9069770f925fdd0855dbd89f1b7678f167c76
kecoma1 <kecoma2@gmail.com>
2021-12-26 18:35:28 +0100

    Improved the stucture of the neural network

--- c++/NeuralNetwork.cpp
+++ c++/NeuralNetwork.cpp
@@ -2,15 +2,15 @@
 #include <cstdio>
 #include <iostream>
 #include <time.h>
-
+#include <math.h>
 
 using namespace std;
 
 class NeuralNetwork {
 	private:
-		short layers;					// Number of layers in the neural network
+		short weightLayers;				// Number of weight layers in the neural network
 		short *layersConfig; 			// Vector with the number of neurons in each layer
-		double **weights;				// Matrix with the weights
+		double ***weights = NULL;		// Vector of matrixes with the weights
 		bool show;						// Attribute to display messages
 
 		/**
@@ -25,32 +25,51 @@ class NeuralNetwork {
 			return min + f * (max - min);
 		}
 
-	public:
 		/**
-		 * @brief Construct a new Neural Network object
+		 * @brief Private method to calculate the sigmoid.
 		 * 
-		 * @param layersConfig Vector with the configuration of
-		 * each layer.
+		 * @param x 
+		 * @return double result.
 		 */
-		NeuralNetwork(short layers,short *layersConfig, bool show=false) {
+		double sigmoid(int x) { return 1/(1+exp(-x)); }
+
+	public:
+		
+		/**
+		 * @brief Construct a new Neural Network object.
+		 * 
+		 * @param weightLayers Number of weight layers (number of weight matrix).
+		 * @param layersConfig 
+		 * @param show 
+		 */
+		NeuralNetwork(short weightLayers, short *layersConfig, bool show=false) {
 			this->layersConfig = layersConfig;
-			this->layers = layers;
+			this->weightLayers = weightLayers;
+
+			for (int i = 0; i < weightLayers+1; i++) printf(" %d ", layersConfig[i]);
+			printf("\n");
 
 			// Initializing the weights
-			this->weights = (double**)malloc(this->layers*sizeof(double*));
+			this->weights = (double***)malloc((this->weightLayers)*sizeof(double**));
 			if (this->weights == NULL)
 				fprintf	(stderr, "[ERROR-1] In the constructor of NeuralNetwork, initializing the weights\n");
 
-			for (short i = 0; i < this->layers; i++) {
-				this->weights[i] = (double*)malloc(this->layersConfig[i]*sizeof(double));
-				if (this->weights[i] == NULL)
-					fprintf(stderr, "[ERROR-2] In the constructor of NeuralNetwork, initializing each weights\n");
+			for (short i = 0; i < this->weightLayers; i++) {
+				if (this->show) printf("Weights %d:\n", i);
 
-				if (this->show) fprintf(stdout, "Weights layer %d:\n", i);
-				for (short n = 0; n < this->layersConfig[i]; n++) {
-					this->weights[i][n] =  this->Rand(-1, 1);
-					if (this->show) fprintf(stdout, " %f ", this->weights[i][n]);
-				if (this->show) fprintf(stdout, "\n");
+				// 1+  because of the bias
+				this->weights[i] = (double**)malloc(1+(this->layersConfig[i])*sizeof(double*));
+				if (this->weights == NULL)
+					fprintf	(stderr, "[ERROR-2] In the constructor of NeuralNetwork, initializing the weights\n");
+
+				for (short n = 0; n < this->layersConfig[i]+1; n++) {
+					this->weights[i][n] = (double*)malloc(this->layersConfig[i+1]*sizeof(double));
+					if (this->weights == NULL)
+						fprintf	(stderr, "[ERROR-3] In the constructor of NeuralNetwork, initializing the weights\n");
+					for (short j = 0; j < this->layersConfig[i+1]; j++) {
+						this->weights[i][n][j] = this->Rand(-1, 1);
+						if (this->show) printf("%d-%d %f\n", n, j, this->weights[i][n][j]);
+					}
 				}
 			}
 		}
@@ -60,18 +79,38 @@ class NeuralNetwork {
 		 * 
 		 */
 		~NeuralNetwork() {
-			for (short i = 0; i < this->layers; i++)
-				free(this->weights[i]);
-			free(this->weights);
 			if (this->show) fprintf(stdout, "Destroying the NeuralNetwork\n");
+			if (this->weights != NULL) {
+				for (short i = 0; i < this->weightLayers; i++) {
+					for (short n = 0; n < this->layersConfig[i]+1; n++) 
+						if (this->weights[i][n] != NULL) 
+							free(this->weights[i][n]);
+					if (this->weights[i] != NULL) free(this->weights[i]);
+				}
+				if (this->weights) free(this->weights);
+			}
 		}
 };
 
 
 int main() {
+	/* Example:
+		2		3			4			1
+		Inputs	LAYER 1 	LAYER 2		OUTPUT
+		X		·			·			·
+				·			·
+		Y		·			·
+							·
+		bias	bias		bias
+
+		This neural network has 2 hidden layers. You need to send to
+		the constructor all the information, number of inputs and number of outputs.
+		We have here 3 weight matrixes.
+	*/
+
 	/* initialize random seed: */
 	srand(time(NULL));
-	short layersConfig[] = {2, 3, 4}; 
-	NeuralNetwork nn(3, layersConfig, true);
+	short layersConfig[] = {2, 3, 1, 1}; // 2 inputs ... 1 output
+	NeuralNetwork nn(3, layersConfig, true); // You have to send the number of layers of weights.
 	return 0;
 }
